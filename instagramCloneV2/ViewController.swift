1ef9d28a1c65148a3818b81f082662952d929f6d
DogukanDGN53 <109498883+dogukandoganios@users.noreply.github.com>
2022-08-03 15:27:37 +0300

    Initial Commit

---
+++ instagramCloneV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagramCloneV2
+//
+//  Created by Doğukan Doğan on 3.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
